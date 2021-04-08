7d5c43621681ac3d5331a1acc9d3658c9ef808c3
Stanislav Petrashov <s@petrashov.ru>
2021-04-08 22:29:49 +0200

    Use Recipe props values to show nutrition

--- web/src/components/Recipe.vue
+++ web/src/components/Recipe.vue
@@ -5,18 +5,18 @@
         {{ name }}
       </p>
     </header>
-<!--    <div class="card-image">-->
-<!--      <figure class="image">-->
-<!--        <img src="../assets/carbonara.jpg" alt="Placeholder image">-->
-<!--      </figure>-->
-<!--    </div>-->
+    <!--    <div class="card-image">-->
+    <!--      <figure class="image">-->
+    <!--        <img src="../assets/carbonara.jpg" alt="Placeholder image">-->
+    <!--      </figure>-->
+    <!--    </div>-->
     <div class="card-content">
       <section>
-        <b-progress :max="141" show-value>
+        <b-progress :max="total" show-value>
           <template #bar>
-            <b-progress-bar type="is-success" :value="35" show-value>P</b-progress-bar>
-            <b-progress-bar type="is-info" :value="38">F</b-progress-bar>
-            <b-progress-bar type="is-warning" :value="68" show-value>C</b-progress-bar>
+            <b-progress-bar type="is-success" :value="protein" show-value>P</b-progress-bar>
+            <b-progress-bar type="is-info" :value="fat">F</b-progress-bar>
+            <b-progress-bar type="is-warning" :value="carbs" show-value>C</b-progress-bar>
           </template>
         </b-progress>
         <b-progress :value="calories" :max="1700" show-value>Calories</b-progress>
@@ -35,6 +35,11 @@ export default {
     fat: Number,
     carbs: Number,
     calories: Number,
+  },
+  computed: {
+    total() {
+      return this.protein + this.fat + this.carbs;
+    },
   },
 };
 </script>
