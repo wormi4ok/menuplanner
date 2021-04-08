77625e0f7a4497829d3253f672cffb0de4d0af8d
Stanislav Petrashov <s@petrashov.ru>
2021-04-08 21:59:28 +0200

    Show empty slots for missing recipes

---
+++ web/src/components/EmptySlot.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <div class="box">
+      Pick me!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptySlot',
+};
+</script>
+
+<style scoped>
+
+</style>
--- web/src/components/Menu.vue
+++ web/src/components/Menu.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="menu">
     <div class="block">
-      <Recipe v-bind="recipes[0]"/>
+      <Recipe v-if="recipes[0]" v-bind="recipes[0]"/>
+      <EmptySlot v-else></EmptySlot>
     </div>
     <div class="block">
-      <Recipe v-bind="recipes[1]"/>
+      <Recipe v-if="recipes[1]" v-bind="recipes[1]"/>
+      <EmptySlot v-else></EmptySlot>
     </div>
     <div class="block">
-      <Recipe v-bind="recipes[2]"/>
+      <Recipe v-if="recipes[2]" v-bind="recipes[2]"/>
+      <EmptySlot v-else></EmptySlot>
     </div>
     <div class="block">
       Summary
@@ -17,11 +20,13 @@
 
 <script>
 import Recipe from '@/components/Recipe.vue';
+import EmptySlot from '@/components/EmptySlot.vue';
 
 export default {
   name: 'Weekday',
   components: {
     Recipe,
+    EmptySlot,
   },
   props: {
     recipes: Object,
