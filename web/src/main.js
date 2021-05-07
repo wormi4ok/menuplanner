565e7cbc876cbaaca8c715b50708e027dddf19af
Stanislav Petrashov <s@petrashov.ru>
2021-05-07 21:42:27 +0200

    Change tabs in mobile vue using swipe gestures

--- web/src/main.js
+++ web/src/main.js
@@ -2,15 +2,17 @@ import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Vue2TouchEvents from 'vue2-touch-events';
 import 'buefy/dist/buefy.css';
 import Buefy from 'buefy';
 import App from './App.vue';
 import store from './store';
+
 import router from './router';
 
 Vue.component('fa', FontAwesomeIcon);
 library.add(fas);
-
+Vue.use(Vue2TouchEvents);
 Vue.use(Buefy, {
   defaultIconComponent: 'fa',
   defaultIconPack: 'fas',
