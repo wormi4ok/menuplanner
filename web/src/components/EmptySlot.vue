621d13817b8cb78a9e5bdab2108dd4a7517351b3
Stanislav Petrashov <s@petrashov.ru>
2021-05-07 22:55:54 +0200

    Make recipe autocomplete input active on click

--- web/src/components/EmptySlot.vue
+++ web/src/components/EmptySlot.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="card">
+    <b-button
+      v-if="!showInput"
+      type="is-success"
+      icon-right="plus-circle"
+      size="is-large" inverted
+      @click="onShowRecipeInput"
+    />
+    <div v-show="showInput" class="has-text-centered">
       <b-field label="Pick a recipe">
         <b-autocomplete
           v-model="name"
           :data="filteredDataObj"
+          ref="recipeInput"
+          type="search"
+          icon="search-plus"
           field="name"
           placeholder="Recipe name"
-          rounded
-          clearable
+          open-on-focus rounded clearable
           @select="pickRecipe">
           <template #empty>No recipes found</template>
         </b-autocomplete>
       </b-field>
+    </div>
   </div>
 </template>
 
@@ -28,6 +39,7 @@ export default {
   },
   data: () => ({
     name: '',
+    showInput: false,
   }),
   computed: {
     filteredDataObj() {
@@ -49,6 +61,10 @@ export default {
     pickRecipe(option) {
       this.$emit('pick-recipe', option);
     },
+    onShowRecipeInput() {
+      this.showInput = true;
+      this.$refs.recipeInput.focus();
+    },
   },
 };
 </script>
@@ -58,5 +74,6 @@ export default {
   padding: 0.75rem 1rem;
   display: flex;
   align-items: center;
+  justify-content: center;
 }
 </style>
