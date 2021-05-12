046524673961f6c6bcfc0b970023aa53855834bd
Stanislav Petrashov <s@petrashov.ru>
2021-05-12 23:32:52 +0200

    Add Login route with Auth form

---
+++ web/src/components/AuthEmailSignIn.vue
@@ -0,0 +1,46 @@
+<template>
+  <form class="block" @submit.prevent="signIn">
+    <b-field label="Email">
+      <b-input
+        v-model="email"
+        icon="envelope"
+        type="email"
+        placeholder="e.g. natalia@flaningo.ru"
+        required
+      />
+    </b-field>
+    <b-field label="Password">
+      <b-input
+        v-model="password"
+        type="password"
+        icon="lock"
+        placeholder="*******"
+        required
+        password-reveal
+      />
+    </b-field>
+    <b-button class="is-primary" label="Continue" native-type="submit"/>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AuthEmailSignIn',
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    signIn() {
+      // submit form here
+      console.log(this.email);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ web/src/components/AuthForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="box">
+    <div class="block">
+      <img src="@/assets/flaningo.jpeg" alt="Menuplanner Logo" width="32" class="mr-4"/>
+      <span class="title has-text-grey">Menuplanner</span>
+    </div>
+    <b-field>
+      <AuthGoogle/>
+    </b-field>
+    <b-collapse :open.sync="showForm" aria-id="loginForm">
+      <template #trigger="props">
+        <div class="has-text-centered">
+          <a v-if="!props.open" aria-controls="loginForm" class="icon-text">
+            <b-icon icon="chevron-down"/>
+            <span>Use Email</span>
+          </a>
+        </div>
+      </template>
+      <b-tabs position="is-centered" size="is-large">
+        <b-tab-item label="Sign In">
+          <AuthEmailSignIn/>
+        </b-tab-item>
+        <b-tab-item label="Sign Up">
+          <AuthEmailSignUp/>
+        </b-tab-item>
+      </b-tabs>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+import AuthGoogle from '@/components/AuthGoogle.vue';
+import AuthEmailSignIn from '@/components/AuthEmailSignIn.vue';
+import AuthEmailSignUp from '@/components/AuthEmailSignUp.vue';
+
+export default {
+  name: 'AuthForm',
+  components: {
+    AuthGoogle,
+    AuthEmailSignIn,
+    AuthEmailSignUp,
+  },
+  data() {
+    return {
+      showForm: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
