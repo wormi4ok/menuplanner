c8b2650691b2f7e664e0c672bfd1df64c72ccfa6
Stanislav Petrashov <s@petrashov.ru>
2021-05-12 23:31:24 +0200

    Add vue-google-oauth2 dependency

--- web/src/main.js
+++ web/src/main.js
@@ -3,6 +3,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Vue2TouchEvents from 'vue2-touch-events';
+import GAuth from 'vue-google-oauth2';
 import 'buefy/dist/buefy.css';
 import Buefy from 'buefy';
 import App from './App.vue';
@@ -16,6 +17,12 @@ Vue.use(Vue2TouchEvents);
 Vue.use(Buefy, {
   defaultIconComponent: 'fa',
   defaultIconPack: 'fas',
+});
+
+Vue.use(GAuth, {
+  clientId: window.config.MP_CLIENT_ID,
+  scope: 'profile email',
+  prompt: 'select_account',
 });
 
 Vue.config.productionTip = false;
