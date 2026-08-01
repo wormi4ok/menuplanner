962ed7feeb7c9310066b4835c017c8ed5cccfdba
Stanislav Petrashov <s@petrashov.ru>
2026-08-01 21:42:48 +0200

    (web): Drop the full Font Awesome set for the 10 icons used

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,17 @@
 import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCheck,
+  faChevronDown,
+  faEdit,
+  faEnvelope,
+  faLock,
+  faPlusCircle,
+  faSearchPlus,
+  faTimes,
+  faTimesCircle,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Vue3TouchEvents from 'vue3-touch-events';
 import 'buefy/dist/css/buefy.css';
@@ -12,7 +23,18 @@ import { init as initGoogleAuth } from './auth/google';
 
 import router from './router';
 
-library.add(fas);
+library.add(
+  faCheck,
+  faChevronDown,
+  faEdit,
+  faEnvelope,
+  faLock,
+  faPlusCircle,
+  faSearchPlus,
+  faTimes,
+  faTimesCircle,
+  faTrash,
+);
 
 if (window.config.MP_CLIENT_ID) {
   initGoogleAuth(window.config.MP_CLIENT_ID);
